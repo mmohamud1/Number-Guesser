534f02a982758ddc54a108ca355357360790a9bf
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-09-24 22:48:53 +0200

    First Commit

---
+++ Number-Guesser-Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Number_Guesser_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
